a05107bb6bf847265ab6ebfb7390c93d05bfbba9
Hack, Kurt J. (GRC-DSB0) <khack@grsla18090370.grc.nasa.gov>
2019-05-02 11:56:18 -0400

    Adding new file MapForItinerary.

---
+++ GitUseExample/MapForItinerary.swift
@@ -0,0 +1,9 @@
+//
+//  MapForItinerary.swift
+//  GitUseExample
+//
+//  Created by Hack, Kurt J. (GRC-DSB0) on 5/2/19.
+//  Copyright © 2019 RocketScience. All rights reserved.
+//
+
+import Foundation
